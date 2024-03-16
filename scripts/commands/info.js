2f0c59662662431a0fbdc9604f75abf7f3c2e45d
EASIRMAHI <136728953+EASIRMAHI@users.noreply.github.com>
2024-03-16 16:43:43 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -28,17 +28,18 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
   
 var callback = () => api.sendMessage({body:`
 --------------------------------------------
-𝐍𝐚𝐦𝐞       : EASIR IQBAL MAHI
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : HOPELESS MAHI
+ACTUALL DEVELOPER : EASIR IQBAL MAHI 
+𝐍𝐚𝐦𝐞       : SABBIR AHMED 
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : SABBIR AHMED
 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
-𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: BAGHA, RAJSHAHI 
-𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: BAGHA, RAJSHAHI 
+𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: BIRGONJ, DINAJPUR 
+𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: BIRGONJ, DINAJPUR 
 𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
 𝐀𝐠𝐞           : 𝟏6+
 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
 𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
-𝐆𝐦𝐚𝐢𝐥       : easiriqbalmahi@gmail.com
-𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801712918055
+𝐆𝐦𝐚𝐢𝐥       : sabbir11222a@gmail.com
+𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801787088373
 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/bankai_mahi
 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : 100072881080249`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
